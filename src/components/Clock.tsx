7484d277926bb228f473f1b2e031ec6f85e834b9
Danil Katz <i_like_it74@mail.ru>
2023-06-18 11:35:13 +0300

    update Clocks

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,16 +1,20 @@
-import { CSSProperties, useEffect, useState } from "react"
+import { CSSProperties } from "react"
 import timeZones from "../time-zones/time-zones";
 type Props = {
     date: Date;
     city: string;
 }
-export const Clock: React.FC<Props> = ({date, city}) => {
+export const Clock: React.FC<Props> = ({ date, city }) => {
     const style: CSSProperties = { display: "flex", flexDirection: "column", alignItems: "center", fontFamily: "cursive", fontSize: 22 };
-    const timeZone: string|undefined = timeZones.find(tz => JSON.stringify(tz).includes(city))?.name ? timeZones.find(tz => JSON.stringify(tz).includes(city))?.name : undefined;
-    const time = date.toLocaleTimeString("en-ES", {timeZone});
+    const currentLocation = date.toString().substring(35, 42);
+
+    const timeZone: string | undefined = timeZones.find(timeZone => JSON.stringify(timeZone).includes(city))?.name;
+    const time = date.toLocaleTimeString("en-US", { timeZone });
+    console.log(timeZone);
+
     return <div style={style}>
         <header>
-            Time in {city}
+            Time in {timeZone ? city : currentLocation}
         </header>
         <p>{time}</p>
     </div>
--- src/components/Clocks.tsx
+++ src/components/Clocks.tsx
@@ -2,17 +2,16 @@ import { useEffect, useState } from "react";
 import { Clock } from "./Clock";
 
 const Clocks: React.FC = () => {
-    
-    const places = ["Yekaterinburg", "Detroit", "Moscow", "German"];  
-   
+    const places: string[] = ["Yekaterinburg", "WWWWWWW", "Detroit", "German"];  
     const [date, setTime] = useState<Date>(new Date());
-
+    
     useEffect(() => {
         const intervalId = setInterval(() => {
             setTime(new Date());
         }, 1000);
         return () => clearInterval(intervalId);
     }, []);
+
     return <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", marginTop: 50}}>
         {/* //${places.map((place) => `<Clock date=${date} city=${place} />`} */}
         <Clock date={date} city={places[0]} />
@@ -20,8 +19,5 @@ const Clocks: React.FC = () => {
         <Clock date={date} city={places[2]} />
         <Clock date={date} city={places[3]} />
     </div>
-}
-function getGmtTime(offset: number) {
-    return new Date(Date.now() - (offset * 60 * 100));
 }
 export default Clocks;
